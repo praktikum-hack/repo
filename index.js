9899e39495f2330cd7e0ddb2a89d62d37967a72d
Oleg Belonovsky <abelo77@yandex.ru>
2021-03-20 13:25:24 +0300

    Initial commit, card number

---
+++ index.js
@@ -0,0 +1,36 @@
+const card = (card_id = 'card') => {
+  const padZero = (v) => {
+    let newValue = `0000${v}`;
+    if (newValue.length > 3) {
+      newValue = newValue.substr(newValue.length - 4, 4);
+    }
+    return newValue;
+  }
+  const cards = document.getElementById(card_id);
+  const cardInputs = cards.querySelectorAll('input');
+  cardInputs.forEach((card) => {
+    card.value = padZero(card.value);
+    card.addEventListener('input', (e)=> {
+      if (!e.target) {
+        return;
+      }
+      const caretPos = e.target.selectionStart;
+      const {value} = e.target;
+      let newValue = value.replace(/\D/g,'');
+      if (newValue.length > 3) {
+        newValue = newValue.substr(0, 4);
+      }
+      e.target.value = newValue;
+      e.target.selectionStart = e.target.selectionEnd = caretPos;
+    });
+    card.addEventListener('blur', (e) => {
+      if (!e.target) {
+        return;
+      }
+      const {value} = e.target;
+      e.target.value = padZero(value);
+    });
+  })
+}
+
+card();
